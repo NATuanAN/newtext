707b02107aa6a6dd690223c03d5e1274de73700b
Your Name <you@example.com>
2024-08-07 22:14:26 +0700

    bt4

--- bt4.cpp
+++ bt4.cpp
@@ -9,24 +9,3 @@ void Time::Xuat()
     cout << "Gio hien tai la: ";
     cout << iGio << ":" << iPhut << ":" << iGiay;
 }
-
-void Time::TinhCongThemMotGiay()
-{
-    iGiay++;
-    if (iGiay >= 60)
-    {
-        iGiay = 0;
-        iPhut++;
-    }
-    if (iPhut >= 60)
-    {
-        iPhut = 0;
-        iGio++;
-    }
-    if (iGio >= 23)
-    {
-        iGio = 0;
-        iPhut = 0;
-        iGiay = 0;
-    }
-}
